501a956f11112682f7e17c3e6016557bd0b0d34b
Benjamín Rolando Feldman <benjafloo2007@gmail.com>
2024-08-19 12:06:20 -0300

    1.0

--- my-app/src/components/interpretacion/interpretacion.jsx
+++ my-app/src/components/interpretacion/interpretacion.jsx
@@ -1,177 +1,189 @@
 import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom'; // Importar useNavigate
+import * as tfjs from '@tensorflow/tfjs';
 import * as tmImage from '@teachablemachine/image';
 import Navbar from '../navbar/navbar';
 import './interpretacion.css';
-import '../img/Button.png'
-import '../img/flechaaas.png'
+import Flechas from './imgs/FlechaCambio.png';
+import Altavoz from './imgs/Altavoz.png';
+import FlechaAtras from './imgs/FlechaAtras.png';
+import Borrar from './imgs/Borrar.png';
+import BotonC from './imgs/BotonCamara.png';
+import BorrarTodo from './imgs/BorrarTodo.png';
+
 const InterpretacionPage = () => {
-    const [model, setModel] = useState(null);
-    const [webcam, setWebcam] = useState(null);
-    const [maxPredictions, setMaxPredictions] = useState(0);
-    const [concatenatedText, setConcatenatedText] = useState('');
-    const [detectedLetters, setDetectedLetters] = useState(new Set());
+  let model, webcam, maxPredictions;
+  const threshold = 0.85;
+  const detectedLetters = new Set();
+  const [concatenatedText, setConcatenatedText] = useState('');
+  const navigate = useNavigate(); // Hook para redirección
 
-    useEffect(() => {
-        loadFromCookies();
-    }, []);
-    const modelURL = "/modelo/model.json";
-    const metadataURL = "/modelo/model.json";
+  useEffect(() => {
+    loadFromCookies(); // Cargar el texto almacenado en cookies al iniciar el componente
+  }, []);
 
-    const init = async () => {
+  const classThresholds = {
+    'Yo': 0.80,
+    'O': 0.95,
+    'I': 0.90,
+    'Como': 0.90,
+    'L': 0.40,
+    'T': 0.70,
+    'Estas': 0.90,
+  };
 
+  async function init() {
+    const modelURL = "./modelo/model.json";
+    const metadataURL = "./modelo/metadata.json";
 
-        const model = await tmImage.load(modelURL, metadataURL);
-        console.log("Model loaded successfully.");
-        setModel(model);
-        setMaxPredictions(model.getTotalClasses());
+    model = await tmImage.load(modelURL, metadataURL);
+    maxPredictions = model.getTotalClasses();
 
-        const webcam = new tmImage.Webcam(324, 185, true);
-        await webcam.setup();
-        await webcam.play();
-        setWebcam(webcam);
+    const flip = true;
+    webcam = new tmImage.Webcam(324, 185, flip);
+    await webcam.setup();
+    await webcam.play();
+    window.requestAnimationFrame(loop);
 
-        document.getElementById("start-area").style.display = "none";
-        document.getElementById("webcam-container").appendChild(webcam.canvas);
-        document.querySelector('.webcam-container').style.display = 'block';
+    document.getElementById("start-area").style.display = "none";
+    document.getElementById("webcam-container").style.display = 'block';
+    document.getElementById("webcam-container").appendChild(webcam.canvas);
 
-        setInterval(() => {
-            document.getElementById("webcam-container").classList.remove('border-green');
-            document.getElementById("webcam-container").classList.add('border-red');
-            predict();
-        }, 1000);
-       setInterval(() => {
-            document.getElementById("webcam-container").classList.remove('border-green');
-            document.getElementById("webcam-container").classList.add('border-red');
-            predict();
-        }, 1000);
-    };
+    setInterval(() => {
+      document.getElementById("webcam-container").classList.remove('border-green');
+      document.getElementById("webcam-container").classList.add('border-red');
+      predict();
+    }, 1000);
+  }
 
-    const predict = async () => {
-        if (!model || !webcam) {
-            console.error("Model or webcam is not initialized");
-            return;
-        }
-        
-        const prediction = await model.predict(webcam.canvas);
-        let highestPrediction = { className: '', probability: 0 };
+  async function loop() {
+    webcam.update();
+    window.requestAnimationFrame(loop);
+  }
 
-        for (let i = 0; i < maxPredictions; i++) {
-            if (prediction[i].probability > highestPrediction.probability) {
-                highestPrediction = prediction[i];
-            }
-        }
+  async function predict() {
+    const prediction = await model.predict(webcam.canvas);
+    let highestPrediction = { className: '', probability: 0 };
 
-        const className = highestPrediction.className;
-        const probability = highestPrediction.probability.toFixed(2);
+    for (let i = 0; i < maxPredictions; i++) {
+      if (prediction[i].probability > highestPrediction.probability) {
+        highestPrediction = prediction[i];
+      }
+    }
 
-        const classThresholds = {
-            'Yo': 0.80,
-            'O': 0.95,
-            'I': 0.30,
-            'Como': 0.35,
-            'L': 0.40,
-            'T': 0.70,
-            'Estas': 0.50,
-        };
+    const className = highestPrediction.className;
+    const classThreshold = classThresholds[className] || threshold;
 
-        const classThreshold = classThresholds[className] || 0.85;
+    if (highestPrediction.probability >= classThreshold && !detectedLetters.has(className)) {
+      detectedLetters.add(className);
+      setConcatenatedText(prevText => {
+        const newText = `${prevText} ${className}`.trim();
+        saveToCookies(newText);
+        return newText;
+      });
+    }
 
-        if (highestPrediction.probability >= classThreshold && !detectedLetters.has(className)) {
-            detectedLetters.add(className);
-            setConcatenatedText(prevText => `${prevText} ${className}`);
-            saveToCookies();
-        }
+    document.getElementById("webcam-container").classList.remove('border-red');
+    document.getElementById("webcam-container").classList.add('border-green');
+  }
 
-        document.getElementById("webcam-container").classList.remove('border-red');
-        document.getElementById("webcam-container").classList.add('border-green');
-    };
+  function removeLastPrediction() {
+    setConcatenatedText(prevText => {
+      const words = prevText.split(' ').filter(word => word.length > 0);
+      if (words.length > 0) {
+        const lastWord = words.pop();
+        detectedLetters.delete(lastWord);
+        const newText = words.join(' ');
+        saveToCookies(newText);
+        return newText;
+      }
+      return prevText;
+    });
+  }
 
-    const removeLastPrediction = () => {
-        setConcatenatedText(prevText => {
-            const newText = prevText.slice(0, -1);
-            detectedLetters.delete(newText[newText.length - 1]);
-            return newText;
-        });
-        saveToCookies();
-    };
+  function clearAllPredictions() {
+    detectedLetters.clear();
+    setConcatenatedText('');
+    saveToCookies('');
+  }
 
-    const clearAllPredictions = () => {
-        setDetectedLetters(new Set());
-        setConcatenatedText('');
-        saveToCookies();
-    };
+  function speakText() {
+    const utterance = new SpeechSynthesisUtterance(concatenatedText);
+    utterance.lang = 'es-ES';
+    window.speechSynthesis.speak(utterance);
+  }
 
-    const speakText = () => {
-        const utterance = new SpeechSynthesisUtterance(concatenatedText);
-        utterance.lang = 'es-ES';
-        window.speechSynthesis.speak(utterance);
-    };
+  function swapText() {
+    const seña = document.querySelector('.switch-label:nth-of-type(1)');
+    const vozTexto = document.querySelector('.switch-label:nth-of-type(2)');
+    const temp = seña.textContent;
+    seña.textContent = vozTexto.textContent;
+    vozTexto.textContent = temp;
+  }
 
-    const swapText = () => {
-        const seña = document.querySelector('.switch-label:nth-of-type(1)');
-        const vozTexto = document.querySelector('.switch-label:nth-of-type(2)');
-        const temp = seña.textContent;
-        seña.textContent = vozTexto.textContent;
-        vozTexto.textContent = temp;
-    };
+  function saveToCookies(text) {
+    document.cookie = `concatenatedText=${text}; path=/; max-age=31536000;`;
+  }
 
-    const saveToCookies = () => {
-        document.cookie = `concatenatedText=${concatenatedText}; path=/; max-age=31536000;`;
-    };
+  function loadFromCookies() {
+    const cookieArr = document.cookie.split(';');
+    for (let i = 0; i < cookieArr.length; i++) {
+      const cookiePair = cookieArr[i].split('=');
+      if (cookiePair[0].trim() === 'concatenatedText') {
+        const text = cookiePair[1] || '';
+        setConcatenatedText(text);
+        text.split(' ').forEach(word => detectedLetters.add(word));
+      }
+    }
+  }
 
-    const loadFromCookies = () => {
-        const cookieArr = document.cookie.split(';');
-        cookieArr.forEach(cookiePair => {
-            const [key, value] = cookiePair.split('=');
-            if (key.trim() === 'concatenatedText') {
-                setConcatenatedText(value);
-            }
-        });
-    };
-
-    return (
-        <div>
-            {/* Implementar el diseño similar al HTML original con JSX */}
-            <img src="Notificatiion bar.png" alt="Arriba" className="Arriba" />
-            <div className="container">
-                <div className="header">
-                    <div className="title">
-                        <img src="arrow-left.png" alt="atras" className="arrow-left" />
-                        <h1 className="interpretacion">Interpretación</h1>
-                    </div>
-                    <p className="interpretation-text">Formato de interpretación</p>
-                    <div className="switch-container">
-                        <div className="switch switch1">
-                            <h1 className="switch-label">Señas</h1>
-                        </div>
-                        <img src="flechaaas.png" alt="Flechas" className="flecha" onClick={swapText} />
-                        <div className="switch switch2">
-                            <h1 className="switch-label">Voz y texto</h1>
-                        </div>
-                    </div>
-                    <div className="buttons">
-                        <div id="start-area" className="start-area">
-                            <button type="button" onClick={init} className="start-button">
-                                <img src="Button.png" alt="Iniciar cámara" className="Boton" />
-                            </button>
-                        </div>
-                        <div id="webcam-container" className="webcam-container"></div>
-                    </div>
-                    <div id="label-container" className="label-container">
-                        <div id="concatenated-text">{concatenatedText}</div>
-                        <img src="Altavoz.png" alt="Altavoz" className="altavoz" onClick={speakText} />
-                        <button className="union-button" onClick={removeLastPrediction}>
-                            <img src="Union.png" alt="Union" className="button-img" />
-                        </button>
-                        <button className="borrar-button" onClick={clearAllPredictions}>
-                            {/* se mueven las imagenes a public */}
-                            <img src="/Borrar.png" alt="Borrar" className="button-img" />
-                        </button>
-                    </div>
-                </div>
+  return (
+    <div>
+    
+      <div className="container">
+        <div className="header">
+          <div className="title">
+          <img
+        src={FlechaAtras}
+        alt="atras"
+        className="arrow-left"
+        onClick={() => navigate('/home')} // Redirige a /home al hacer clic
+        />
+            <h1 className="interpretacion">Interpretación</h1>
+          </div>
+          <p className="interpretation-text">Formato de interpretación</p>
+          <div className="switch-container">
+            <div className="switch switch1">
+              <h1 className="switch-label">Señas</h1>
             </div>
+            <img src={Flechas} alt="Flechas" className="flecha" onClick={swapText} />
+            <div className="switch switch2">
+              <h1 className="switch-label">Voz y texto</h1>
+            </div>
+          </div>
+          <div className="buttons">
+            <div id="start-area" className="start-area">
+              <button type="button" onClick={init} className="start-button">
+                <img src={BotonC} alt="Iniciar cámara" className="Boton" />
+              </button>
+            </div>
+            <div id="webcam-container" className="webcam-container" style={{ display: 'none' }}></div>
+          </div>
+          <div id="label-container" className="label-container">
+            <div id="concatenated-text">{concatenatedText}</div>
+            <img src={Altavoz} alt="Altavoz" className="altavoz" onClick={speakText} />
+            <button className="union-button" onClick={removeLastPrediction}>
+              <img src={Borrar} alt="Borrar" className="button-img" />
+            </button>
+            <button className="borrar-button" onClick={clearAllPredictions}>
+              <img src={BorrarTodo} alt="Borrar todo" className="button-img" />
+            </button>
+          </div>
         </div>
-    );
+      </div>
+      <Navbar />
+    </div>
+  );
 };
 
 export default InterpretacionPage;
--- my-app/src/components/navbar/navbar.jsx
+++ my-app/src/components/navbar/navbar.jsx
@@ -1,37 +1,46 @@
 import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import './navbar.css';
-import { Link } from 'react-router-dom';
 import Home from '../img/Home.png';
 import interpretew from '../img/Interprete-white.png';
 import enseñanza from '../img/Ensenanza-white.png';
 import emergencia from '../img/Emergencia-white.png';
 import perfil from '../img/Perfil-white.png';
+
 const Navbar = () => {
+  const location = useLocation();
+
   return (
     <nav className="custom-navbar">
-      <div className="nav-item">
+      <div className={`nav-item ${location.pathname === '/home' ? 'active' : ''}`}>
         <Link to="/home">
-      <img src={Home} alt="profile" className="Home" />
-        <span className="nav-text active">Home</span>
+          <img src={Home} alt="home" className="Home" />
+          <span className="nav-text">Home</span>
         </Link>
       </div>
-      <div className="nav-item">
-      <Link to="/interprete">
-      <img src={interpretew} alt="profile" className="Interprete" />
-        <span className="nav-text">Interprete</span>
+      <div className={`nav-item ${location.pathname === '/interprete' ? 'active' : ''}`}>
+        <Link to="/interprete">
+          <img src={interpretew} alt="interprete" className="Interprete" />
+          <span className="nav-text">Interprete</span>
         </Link>
       </div>
-      <div className="nav-item">
-      <img src={enseñanza} alt="profile" className="Enseñanza" />
-        <span className="nav-text">Enseñanza</span>
+      <div className={`nav-item ${location.pathname === '/enseñanza' ? 'active' : ''}`}>
+        <Link to="/enseñanza">
+          <img src={enseñanza} alt="enseñanza" className="Enseñanza" />
+          <span className="nav-text">Enseñanza</span>
+        </Link>
       </div>
-      <div className="nav-item">
-      <img src={emergencia} alt="profile" className="Emergencia" />
-        <span className="nav-text">Emergencia</span>
+      <div className={`nav-item ${location.pathname === '/emergencia' ? 'active' : ''}`}>
+        <Link to="/emergencia">
+          <img src={emergencia} alt="emergencia" className="Emergencia" />
+          <span className="nav-text">Emergencia</span>
+        </Link>
       </div>
-      <div className="nav-item">
-      <img src={perfil} alt="profile" className="Perfil" />
-        <span className="nav-text">Perfil</span>
+      <div className={`nav-item ${location.pathname === '/perfil' ? 'active' : ''}`}>
+        <Link to="/perfil">
+          <img src={perfil} alt="perfil" className="Perfil" />
+          <span className="nav-text">Perfil</span>
+        </Link>
       </div>
     </nav>
   );
