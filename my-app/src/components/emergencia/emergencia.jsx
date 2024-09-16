7f0d4c7626c40d3e0231a219592e80a151dba6d6
Benjamín Rolando Feldman <benjafloo2007@gmail.com>
2024-09-16 09:32:17 -0300

    2.1.1

--- my-app/src/components/emergencia/emergencia.jsx
+++ my-app/src/components/emergencia/emergencia.jsx
@@ -22,14 +22,23 @@ const EmergenciaScreen = () => {
         </h2>
       </div>
       <div className={styles.direccion}> 
-        <h2 className={styles.Ubi}>
+        <h4 className={styles.Ubi}>
           <img src={Location} alt="Location" className={styles.location}/> 
           Ubicación actual
-        </h2>
-        <h2 className={styles.dire}> {/* Cambié `Dire` por `dire` para que coincida con el CSS */}
+        </h4>
+        <h3 className={styles.dire}> {/* Cambié `Dire` por `dire` para que coincida con el CSS */}
           Av. 9 de Julio 8319, CABA
-        </h2>
+        </h3>
       </div>
+      <div className={styles.Textazo}>
+      <h2 className={styles.textoo}> ¿Estás en una emergencia?</h2>
+      <h4 className={styles.texto2}>Presiona el botón de SOS </h4>
+      <h4 className={styles.texto2}>por 3 segundos. Se avisará </h4>
+      <h4 className={styles.texto2}>a tus contactos y al centro </h4>
+      <h4 className={styles.texto2}>de emergencia más </h4>
+      <h4 className={styles.texto2}>cercano.</h4>
+      </div>
+
     </div>
   );
 };
